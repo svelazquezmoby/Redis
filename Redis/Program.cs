8679ebf650ae04e7409cfbe355d8db24c769a215
sergio351 <velazquezsergio714@mail.com>
2023-01-27 15:38:31 -0300

    redis

---
+++ Redis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using StackExchange.Redis;
+
+
+
+namespace Redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redisDB = RedisDB.Connection.GetDatabase();
+            redisDB.StringSet("1", "cerveza");
+
+            var valor = redisDB.StringGet("1");
+            Console.WriteLine(valor);
+        }
+    }
+
+    public class RedisDB
+    {
+        private static Lazy<ConnectionMultiplexer> _lazyconnection;
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return _lazyconnection.Value;
+            }
+
+        }
+        static RedisDB()
+        {
+            _lazyconnection = new Lazy<ConnectionMultiplexer>(() =>
+            ConnectionMultiplexer.Connect("localhost")
+            ); 
+        }
+    }
+}
